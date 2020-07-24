71229b2e5a9c8f73065965e919311ee23e06ac36
ging-dev <gingdz11012001@gmail.com>
2020-07-24 17:31:49 +0700

    Maintain

--- src/ginglib.cpp
+++ src/ginglib.cpp
@@ -5,16 +5,18 @@
 bool GingLib::isPrime(int x)
 {
     int i, n = sqrt(x);
-    if (x > 1) {
-        for (i = 2; i <= n; i++)
-            if (x%i == 0) return false;
-        return true;
-    } else return false;
+    if (x < 2)
+        return false;
+    for (i = 2; i <= n; i++)
+        if (x%i == 0)
+            return false;
+    return true;
 }
 
 // Kiểm tra số chính phương
 bool GingLib::isSquare(int x)
 {
-    if (pow(sqrt(x), 2) == x) return true;
+    if (pow(sqrt(x), 2) == x)
+        return true;
     return false;
 }
